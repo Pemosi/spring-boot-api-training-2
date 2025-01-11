37eb8d02752492dbd94e471a7013e0f9dcd336f1
Pemosi <130945525+Pemosi@users.noreply.github.com>
2025-01-11 15:25:50 +0900

    エラーハンドリングを実装 (#3)

--- api-demo/src/main/java/com/example/springboot/api_demo/controller/UserController.java
+++ api-demo/src/main/java/com/example/springboot/api_demo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springboot.api_demo.dto.UserRequestDto;
-import com.example.springboot.api_demo.dto.UserResponseDto;
+import com.example.springboot.api_demo.dto.user.UserRequestDto;
+import com.example.springboot.api_demo.dto.user.UserResponseDto;
 import com.example.springboot.api_demo.usecase.UserUsecase;
 
 @RestController
@@ -34,7 +35,7 @@ public class UserController {
     }
 
 	@PostMapping
-    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto dto) {
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody @Validated UserRequestDto dto) {
 		final UserResponseDto user = this.usecase.addUser(dto);
 		return ResponseEntity.ok(user);
     }
